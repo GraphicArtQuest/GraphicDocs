e66a8dd28f0e9f9bb7d82edb6ea521c96799cb4e
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-11-17 09:10:32 -0800

    docs(parser): improved `get_author` documentation

--- src/parse_docstring_functions/get_authors.py
+++ src/parse_docstring_functions/get_authors.py
@@ -1,9 +1,23 @@
-def get_authors(docstring: str) -> None:
-    """Goes through the docstring and looks for authors annotated by the @author tag"""
-    
+def get_authors(docstring: str) -> dict[str | None, str | None] | None:
+    """
+        Goes through the docstring and looks for authors annotated by the @author tag.
+
+        If no authors are found, it returns `None`. Otherwise, it returns a list of dictionaries with keys
+        `name` and `email`. Either author name or email may be left blank, which will return that key as `None`.
+        The email value is everything between a set of brackets.
+
+        For example:
+        - `@author John Doe`
+            - Returns: `{"name": "John Doe", "email": None}`
+        - `@author John Doe [John.Doe@myemail.com]`
+            - Returns: `{"name": "John Doe", "email": "john.doe@somedomain.com"}`
+        - `@author [john.doe@somedomain.com]`
+            - Returns: `{"name": None, "email": "john.doe@somedomain.com"}`
+    """
+
     parsed = docstring.splitlines()
-    
-    authors_array = []
+
+    authors = []
     author_name = ""
     author_email = ""
 
@@ -11,7 +25,7 @@ def get_authors(docstring: str) -> None:
         stripped_line = line.strip()
 
         if stripped_line[0:8] == "@author ":
-            # We have encountered a new parameter, start recording the info
+            # We have encountered a new author, start recording the info
             email_start = stripped_line.find("[", 9)
             email_end = stripped_line.find("]", 9)
 
@@ -22,8 +36,8 @@ def get_authors(docstring: str) -> None:
                 author_name = stripped_line[8:len(stripped_line)].strip()
                 author_email = None
 
-            authors_array.append({"name": author_name, "email": author_email})
+            authors.append({"name": author_name, "email": author_email})
     
-    if len(authors_array) > 0:
-        return authors_array
+    if len(authors) > 0:
+        return authors
     return None
