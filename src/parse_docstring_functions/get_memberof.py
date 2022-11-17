884c0cbabfc53684ab2e193baafeba97d3b30b4a
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-11-17 09:52:42 -0800

    docs(parser): improved `get_memberof` documentation

--- src/parse_docstring_functions/get_memberof.py
+++ src/parse_docstring_functions/get_memberof.py
@@ -1,29 +1,33 @@
-def get_memberof(docstring: str) -> None:
+def get_memberof(docstring: str) -> list[str] | None:
     """
     Goes through the docstring and looks for the `@memberof` tag.
     
     This tag is for a single line only, and takes one argument: the member name following the tag. This name must
-    be a valid Python variable name. Only unique names are added to the list.
+    be a valid Python variable name.
     
-    If the tag is not included or is invalid, it returns `None`.
+    If the tag is not included or is invalid, it returns `None`. Otherwise, it returns a list of unique member strings.
+    
+    For example:
+    - `@memberof Tools`
+    - `@memberof Tools.Wrenches`
     """
 
     parsed = docstring.splitlines()
 
-    member_array = []
+    membersof = []
 
     for line in parsed:
         stripped_line = line.strip()
 
         if stripped_line[0:10] == "@memberof ":
-            
+
             member = stripped_line[10:len(line)].strip()
-            
+
             if member.isidentifier():
-                if member not in member_array:  # Only add unique members
-                    member_array.append(member)
+                if member not in membersof:  # Only add unique members
+                    membersof.append(member)
             continue
-    
-    if len(member_array) > 0:
-        return member_array
+
+    if len(membersof) > 0:
+        return membersof
     return None
