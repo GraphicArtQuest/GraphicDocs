4fb99eae9931f7ae3a10c80e24dbef039767c5ec
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-11-17 09:36:56 -0800

    docs(parser): improved `get_global` documentation

--- src/parse_docstring_functions/get_global.py
+++ src/parse_docstring_functions/get_global.py
@@ -1,11 +1,16 @@
-def get_global(docstring: str) -> None:
-    """Goes through the doc string and looks for a @global tag"""
-    
+def get_global(docstring: str) -> bool:
+    """
+        Goes through the docstring and looks for a @global tag. This indicates that this should be treated as part of
+        the global namespace, regardless of where it was found in the code.
+
+        If found, it returns `True`. If omitted, it returns `False`.
+    """
+
     parsed = docstring.splitlines()
 
     for line in parsed:
 
         if line.strip() == "@global":
             return True
-    
+
     return False
