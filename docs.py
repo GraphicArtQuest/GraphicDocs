8730026f21332c6e55f7dd55661736fe2b2a5806
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2023-01-10 12:05:01 -0800

    feat(docs): create a project documentation script
    This script uses its own parser and core to build API documentation.

---
+++ docs.py
@@ -0,0 +1,16 @@
+from src.core import Core
+
+config = {
+    "source": [".\\src"],
+    "source_depth": 1,
+    "source_exclude_pattern": ["__init__"],
+    "destination": ".\\docs\\api",
+    "destination_overwrite": True,
+    "graphic_md":
+        {
+            "footer": "Visit [Graphic Art Quest](www.GraphicArtQuest.com) for more!"
+        }
+}
+
+core = Core(config)
+core.build()
