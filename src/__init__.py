3174e2bcd715199e236206db74d3e3aaed111033
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2023-01-10 13:17:34 -0800

    feat: add explicit import references when importing the source files

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from src.core import Core, ConsoleColorCodes, FormatForConsole
+from src.hooks import HookException, Hooks
+from src.parser import parse_module, parse_class, parse_function, parse_docstring
